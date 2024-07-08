3d7c60c451bc02390b86462057051835779d6d22
SharanyaKalirajan <164001667+SharanyaKalirajan@users.noreply.github.com>
2024-07-08 21:34:46 +0530

    Create post.service.spec.ts

---
+++ post.service.spec.ts
@@ -0,0 +1,11 @@
+import { TestBed } from '@angular/core/testing';
+import { PostService } from './post.service'; 
+describe('PostService', () => { 
+ beforeEach(() => { 
+ TestBed.configureTestingModule({}); 
+ service = TestBed.inject(PostService); 
+ }); 
+ it('should be created', () => { 
+ expect(service).toBeTruthy(); 
+ }); 
+}); 
